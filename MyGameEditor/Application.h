f8f0aafe699b9200f080fdb867aa9e8580819940
PauFusco <99730051+PauFusco@users.noreply.github.com>
2023-10-16 21:28:11 +0200

    tried to add ui
    spoiler: it exploded

--- MyGameEditor/Application.h
+++ MyGameEditor/Application.h
@@ -1,12 +1,14 @@
 #include "Globals.h"
 #include "Module.h"
 #include "Window.h"
+#include "UI.h"
 
 #include <list>
 
 class Application {
 public:
 	Window* window;
+	UI* ui;
 
 private:
 	std::list<Module*> list_modules;
