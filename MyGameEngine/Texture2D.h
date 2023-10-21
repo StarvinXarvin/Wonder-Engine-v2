e37dda4eda6c6bbb80e92ab4fa7ce0278472a156
StarvinXarvin <99918767+StarvinXarvin@users.noreply.github.com>
2023-10-21 19:07:12 +0200

    Added Mesh.cpp and Mesh.h

--- MyGameEngine/Texture2D.h
+++ MyGameEngine/Texture2D.h
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
 class Texture2D
 {
+
+private:
 	unsigned int _id;
 
 public:
-
-	inline unsigned int id() const { return _id; }
-
+	using Ptr = std::shared_ptr<Texture2D>;
 	explicit Texture2D(const std::string& path);
-	~Texture2D();
-	
+	Texture2D(Texture2D&& tex) noexcept;
 	void bind() const;
+	~Texture2D();
+
+private:
+	Texture2D(const Texture2D&);
+	Texture2D operator=(const Texture2D&);
+
 };
+
 
