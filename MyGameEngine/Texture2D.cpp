7a8b6543bd6552a0e262f16147554e475926b871
StarvinXarvin <99918767+StarvinXarvin@users.noreply.github.com>
2023-10-22 19:20:01 +0200

    House texture added

--- MyGameEngine/Texture2D.cpp
+++ MyGameEngine/Texture2D.cpp
@@ -9,7 +9,7 @@ Texture2D::Texture2D(const std::string& path) {
     //load image data using devil
     auto img = ilGenImage();
     ilBindImage(img);
-    ilLoadImage("Lenna.png");
+    ilLoadImage("Assets/Baker_house.png");
     auto width = ilGetInteger(IL_IMAGE_WIDTH);
     auto height = ilGetInteger(IL_IMAGE_HEIGHT);
     auto channels = ilGetInteger(IL_IMAGE_CHANNELS);
